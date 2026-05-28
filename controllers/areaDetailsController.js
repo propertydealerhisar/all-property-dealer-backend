4067ab292ea0af5f09ce2da3dc4e9ef90b2d8e0e
Mukesh Kumar <mk8701952@gmail.com>
2026-05-28 12:06:25 +0530

    upadte

--- controllers/areaDetailsController.js
+++ controllers/areaDetailsController.js
@@ -26,3 +26,43 @@ exports.getLocationsByCity = async (req, res) => {
     });
   }
 };
+
+
+//=======================================
+exports.getAllLocationsForSitemap = async (req, res) => {
+  try {
+    const locations = await AreaDetailes.find({})
+      .select("city location slug -_id")
+      .lean();
+
+    const groupedData = {};
+
+    locations.forEach((item) => {
+      const city = item.city?.trim();
+
+      if (!groupedData[city]) {
+        groupedData[city] = [];
+      }
+
+      // ✅ slug ke end se -haryana remove
+      const cleanSlug = item.slug?.replace(
+        /-haryana$/i,
+        ""
+      );
+
+      groupedData[city].push(cleanSlug);
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: groupedData,
+    });
+  } catch (error) {
+    console.log("Sitemap API Error =>", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
--- routes/areaDetailes.js
+++ routes/areaDetailes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  getLocationsByCity,
+  getLocationsByCity,getAllLocationsForSitemap
 } = require("../controllers/areaDetailsController");
 
 router.get("/locations/:city", getLocationsByCity);
+router.get("/sitemap-locations", getAllLocationsForSitemap);
 
 module.exports = router;
